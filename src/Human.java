6ba0e8e07c3712bb2eeac5c5b9cf747e1a4b5ed1
maleckyy <kupferpatryk@gmail.com>
2020-03-03 15:46:23 +0100

    2

---
+++ src/Human.java
@@ -0,0 +1,6 @@
+public class Human {
+    String firstName;
+    String lastName;
+    Phone phone;
+    Animal pet;
+}
---
+++ src/Main.java
@@ -0,0 +1,27 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        Human me = new Human();
+        me.firstName = "Kacper";
+        me.lastName = "Warda";
+        me.pet = new Animal("mouse");
+        me.pet.name = "Myszojeleń";
+
+        me.pet.feed();
+        me.pet.feed();
+        me.pet.feed();
+        me.pet.feed();
+        me.pet.takeForAWalk();
+        me.pet.takeForAWalk();
+        me.pet.feed();
+        me.pet.feed();
+        me.pet.takeForAWalk();
+        me.pet.takeForAWalk();
+        me.pet.takeForAWalk();
+        me.pet.takeForAWalk();
+        me.pet.takeForAWalk();
+        me.pet.feed();
+
+    }
+}
